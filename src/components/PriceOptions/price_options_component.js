5c49d5d8e564862af7d0f920977ed183e231c1fd
Ivan Trujillo <krival1987@gmail.com>
2018-02-17 15:32:37 -0500

    completed the rest of the cards and prices, added zindex for ttiles to show infront of the image, added green button styles, effects and font

--- src/components/PriceOptions/price_options_component.js
+++ src/components/PriceOptions/price_options_component.js
@@ -5,7 +5,7 @@ const PriceOptions = () =>{
   <section className="section-tours">
    <div className="u-center-text u-margin-bottom-big">
     <h2 className="heading-secondary">
-     Doggie Loves!
+     They will join you
     </h2>
    </div>
    <div className="row">
@@ -31,8 +31,17 @@ const PriceOptions = () =>{
       </div>
       </div>
       <div className="card__side card__side--back card__side--back-1">
-
-       BACK
+       <div className="card__cta">
+        <div className="card__price-box">
+         <p className="card__price-only">
+           Only
+         </p>
+          <p className="card__price-value">
+           $297
+          </p>
+        </div>
+        <a href="#back" className="btn btn--white">Book Now</a>
+       </div>
       </div>
      </div>
       </div>
@@ -42,15 +51,27 @@ const PriceOptions = () =>{
        <div className="card__picture card__picture--2">
          &nbsp;
        </div>
-       <div className="card__heading">
-         Heading
-       </div>
+       <h4 className="card__heading">
+         <span className="card__heading-span card__heading-span--2">The Forest Hiker</span>
+       </h4>
       <div className="card__details">
-        Details
+        <ul>
+         <li> 7 day tour</li>
+         <li> Up to 40 people</li>
+         <li> 6 tour guides</li>
+         <li> Sleep in provided tents</li>
+         <li> Difficulty:medium </li>
+        </ul>
       </div>
       </div>
       <div className="card__side card__side--back card__side--back-2">
-     BACK
+        <div className="card__cta">
+         <div className="card__price-box">
+          <p className="card__price-only">Only</p>
+           <p className="card__price-value">$497</p>
+         </div>
+         <a href="#ack" className="btn btn--white">Book Now</a>
+        </div>
       </div>
      </div>
       </div>
@@ -60,18 +81,33 @@ const PriceOptions = () =>{
        <div className="card__picture card__picture--3">
          &nbsp;
        </div>
-       <div className="card__heading">
-         Heading
+       <h4 className="card__heading">
+        <span className="card__heading-span card__heading-span--3">The Snow Adventurer</span>
+       </h4>
+       <div className="card__details">
+        <ul>
+         <li> 5 day tour </li>
+         <li> Up to 15 people </li>
+         <li> 3 tour guides </li>
+         <li> Sleep in provided tents </li>
+         <li> Difficult:hard </li>
+        </ul>
        </div>
-      <div className="card__details">
-        Details
-      </div>
       </div>
       <div className="card__side card__side--back card__side--back-3">
-       BACK!
+       <div className="card__cta">
+       <div className="card__price-box">
+        <p className="card__price-only">Only</p>
+        <p className="card__price-value">$897</p>
+       </div>
+        <a href="#forward" className="btn btn--white">Book Now!</a>
+       </div>
       </div>
      </div>
       </div>
+   </div>
+   <div className="u-center-text u-margin-top-big">
+    <a href="#tours" className="btn btn--green">Discover All Tours</a>
    </div>
   </section>
  )
